5c4ea50408d2de6698900cf933a730eb20e50ba3
Whitebowfr <69417022+Whitebowfr@users.noreply.github.com>
2021-03-13 20:18:00 +0100

    Update with base components since it works

--- index.js
+++ index.js
@@ -12,52 +12,37 @@ const { Server } = require('ws');
 const wss = new Server({ server });
 
 var clientID = 0
-//var data = readDatabase()
+var data = readDatabase()
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
-    ws.onmessage = function(evt) {
-        console.log(evt.data)
+wss.on('connection', (ws, req) => {
+
+    function sendM(message) {
+        ws.send(message);
     }
-  });
 
+    console.log(data.connectedIP, data.connectedIP.toString().indexOf(req.connection.remoteAddress.toString()))
+    if (data.connectedIP.indexOf(req.connection.remoteAddress) > -1) {
+        sendM("ID :" + data.correspondingID[data.connectedIP.indexOf(req.connection.remoteAddress)].toString())
+    } else {
+        clientID = Math.round(Math.random() * 1000)
+        data.connectedIP.push(req.connection.remoteAddress.toString())
+        data.correspondingID.push(clientID)
+        sendM("ID : " + clientID)
+    }
+    updateDatabase(data)
+    
+    ws.on('close', () => console.log('Client disconnected'));
 
+    ws.onmessage = function(evt) { 
+        console.log('received: %s', evt.data, " from ", req.connection.remoteAddress,);
 
-  setInterval(() => {
-    wss.clients.forEach((client) => {
-      client.send(new Date().toTimeString());
-    });
-  }, 1000);
-
-/*wss.on('connection', function connection(ws, req) {
-    var isMessage = false
- 
-    ws.on('message', function incoming(message) {
-        isMessage = true
-        console.log('received: %s', message, " from ", req.connection.remoteAddress,);
-        if (message == "newConnection") {
-            console.log(data.connectedIP, data.connectedIP.toString().indexOf(req.connection.remoteAddress.toString()))
-            if (data.connectedIP.indexOf(req.connection.remoteAddress) > -1) {
-                sendM("ID :" + data.correspondingID[data.connectedIP.indexOf(req.connection.remoteAddress)].toString())
-            } else {
-                clientID = Math.round(Math.random() * 1000)
-                data.connectedIP.push(req.connection.remoteAddress.toString())
-                data.correspondingID.push(clientID)
-                sendM("ID : " + clientID)
-            }
-            updateDatabase(data)
-        }
-        if (message.toString().includes("__")) {
+        
+        if (evt.data.toString().includes("__")) {
             var messageCommand = message.split("__")
             if (messageCommand[0] == "f") {
                 eval(messageCommand[1])
             }
         }
-    });
-
-    function sendM(message) {
-        ws.send(message);
     }
 });
 
@@ -71,4 +56,4 @@ function readDatabase() {
 
 function updateDatabase(dataToWrite) {
     fs.writeFileSync("database.json", JSON.stringify(dataToWrite))
-}*/
+}
